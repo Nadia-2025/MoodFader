8c92ab534b7aafaa5b3ca3722bd3e715ef247d00
Nadia-2025 <nadyah-88@hotmail.com>
2025-11-29 21:38:19 +0100

    Añadir endpoints para moods_routes y rectificar el modelo users

---
+++ backend/models/users.py
@@ -0,0 +1,21 @@
+from database import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+  __tablename__="users"
+
+  id = db.Column(db.Integer, primary_key =True)
+  email = db.Column(db.String(120), unique =True, nullable=False)
+  username = db.Column(db.String(50), unique=True, nullable=False)
+  password_hash = db.Column(db.String, nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+  favourites = db.relationship("Favourite", backref="user", lazy=True)
+  moods = db.relationship("Mood", backref="user", lazy=True)
+
+  def set_password(self,password):
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self,password):
+    return check_password_hash (self.password_hash, password)
--- backend/routes/moods_routes.py
+++ backend/routes/moods_routes.py
@@ -1,13 +1,13 @@
-
 from flask import Blueprint, request, jsonify
 from spotify import search_playlists_by_mood
+from database import db
+from models.mood import Mood
+from models.users import User
+import jwt
+import os
 
 mood_bp = Blueprint("mood_bp", __name__)
-
-@mood_bp.route("/intro")
-def intro():
-  return jsonify ({"msg": "Hola!!!"})
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 @mood_bp.route('/mood', methods=['GET'])
 def get_mood_playlists():
@@ -23,6 +23,29 @@ def get_mood_playlists():
         "emotion": mood,
         "playlists": playlists
     })
+
+@mood_bp.route("/mood/save", methods=["POST"])
+def save_mood():
+    token = request.headers.get("Authorization")
+    if not token:
+        return jsonify({"error": "Missing token"}), 401
+
+    try:
+        payload = jwt.decode(token.split(" ")[1], SECRET_KEY, algorithms=["HS256"])
+        user_id = payload["user_id"]
+    except Exception as e:
+        return jsonify({"error": "Invalid token"}), 401
+
+    data = request.json
+    mood_name = data.get("mood_name")
+    if not mood_name:
+        return jsonify({"error": "Mood name required"}), 400
+
+    new_mood = Mood(user_id=user_id, mood_name=mood_name)
+    db.session.add(new_mood)
+    db.session.commit()
+
+    return jsonify({"message": "Mood saved successfully"})
 
 # @bp.route('/callback')
 # def callback():
